fd94826070a43f7ae16e61aab3f9623a9477c668
Vanessa Felix Christoff <vfchristoff9@gmail.com>
2023-06-25 19:00:03 -0300

    Toggle menu for mobile

---
+++ togglemenu.js
@@ -0,0 +1,11 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    nav-links.classList.toggle("active");
+})
+
+document.querySelectorAll("nav-links").forEach(n=> n.addEventListener( "click",  () =>{
+
+}))
