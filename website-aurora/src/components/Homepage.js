aaaad23795176a1f3ab698ab6dceeaa31ff20ee1
Vanessa Felix Christoff <vfchristoff9@gmail.com>
2024-01-14 00:13:08 -0300

    Homepage.js component fix responsive for multiple screens

--- website-aurora/src/components/Homepage.js
+++ website-aurora/src/components/Homepage.js
@@ -12,8 +12,11 @@ const Homepage = () => {
                 <div className="text-center text-white">
                     <h2 className="font-heebo-sans text-6xl drop-shadow-md">Leading in Tech</h2>
                     <br />
-                    <h2 className="font-heebo-sans ">We are the future of the semiconductor industry. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</h2>
+                    <br />
+                    <h2 className="px-12 sm:px-16 md:px-20 lg:px-40 xl:px-60 2xl:px-80">
+                        We are the future of the semiconductor industry. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod consectetur adipiscing elit, sed do eiusmod consectetur adipiscing elit, sed do eiusmod</h2>
+                    <br />
                     <br />
                     <button className="font-heebo-sans text-sm lg:text-lg border border-white py-2 px-4 hover:bg-orange-500 transition duration-300 ease-in-out">
                         Contact us if you want to be part of something big
