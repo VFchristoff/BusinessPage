73ccacf1557c28fdcbce3d1bc3e683fabca091b1
Vanessa Felix Christoff <vfchristoff9@gmail.com>
2024-01-13 23:54:54 -0300

    Navbar.js component and images

--- website-aurora/src/App.js
+++ website-aurora/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import About from './components/About';
+// import Experience from './components/Experience';
+// import Projects from './components/Projects';
+// import Contact from './components/Contact';
+// import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Homepage from './components/Homepage';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <Navbar />
+        <Homepage />
+        {/*<Skills />*/}
+        {/*<Experience />*/}
+        {/*<Projects />*/}
+        {/*<Contact />*/}
+        {/*<Footer />*/}
+      </div>
   );
-}
+};
 
 export default App;
+
