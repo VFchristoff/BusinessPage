5c6e1302f401542d5c475d82387a1a2fb8b066a7
Vanessa Felix Christoff <vfchristoff9@gmail.com>
2024-01-15 05:30:01 -0300

    Dynamic images component DynamicColumnsBox_Image.js

---
+++ website-aurora/src/components/DynamicColumnsBox_Image.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const DynamicColumnsBox_Image = ({ title, imageUrls }) => {
+    return (
+        <div className="flex flex-wrap justify-center px-6 sm:px-16 md:px-10 lg:px-20 xl:px-40 2xl:px-60">
+            {title.map((_, index) => (
+                <div key={index} className={`w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4 mb-4 ${index !== title.length - 1 ? 'md:mx-6' : ''}`}>
+                    <p className="text-center font-semibold text-2xl pb-3 text-gray-800">{title[index]}</p>
+                    <img src={imageUrls[index]} alt={`Image ${index + 1}`} className="object-cover h-48 w-full rounded-md mb-4 transition duration-500 ease-in-out transform hover:scale-105" />
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default DynamicColumnsBox_Image;
